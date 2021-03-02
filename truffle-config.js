0ff7b7252600ea45949d87ffde6837a0d70c0f7e
smartsir796 <smartsir796@adshares.net>
2021-03-02 19:07:44 +0100

    cleanup

--- truffle-config.js
+++ truffle-config.js
@@ -24,9 +24,6 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
-const privKeyrinkeby = "08795dd84af4d4c28104c6186a6581f39ac356c3df0bd6b4715cfa9c9df419f0"
-const PrivateKeyProvider = require("truffle-privatekey-provider");
-
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -61,15 +58,15 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-     ropsten: {
+     //ropsten: {
     // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-        provider: () => new PrivateKeyProvider(privKeyrinkeby, "https://ropsten.infura.io/metamask"),
-         network_id: 3,       // Ropsten's id
-         gas: 5500000,        // Ropsten has a lower block limit than mainnet
-         confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-         timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-         skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-     },
+    //    provider: () => new PrivateKeyProvider(privKeyrinkeby, "https://ropsten.infura.io/metamask"),
+    //     network_id: 3,       // Ropsten's id
+    //     gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    //     confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    //     timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    //     skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // },
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
